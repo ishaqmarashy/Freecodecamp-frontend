c73c1906969f4897fe33ee034ff23ab3c14f86e9
ishaqmarashy <34040118+ishaqmarashy@users.noreply.github.com>
2023-08-27 23:03:27 +0400

    Add hljs
    Add markdown library for code syntax markdown

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,6 +1,8 @@
 import React,{useState} from "react";
-import {marked} from 'marked';
-
+import {Marked} from "marked";
+import {markedHighlight} from "marked-highlight";
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.css';
 const initialMarkdown=`# My Awesome Markdown Previewer!
 
 ## An Exciting Sub-Heading
@@ -8,7 +10,7 @@ const initialMarkdown=`# My Awesome Markdown Previewer!
 
 Here's an example of code: \`<span></span>\`, nestled between backticks.
 
-\`\`\`
+\`\`\`js
 // Multi-line code example:
 
 function demoCode(firstLine, lastLine) {
@@ -44,32 +46,52 @@ Row 2, Col 1 | Row 2, Col 2 | Row 2, Col 3
 ![Example Image](https://via.placeholder.com/150)
 `;
 
-
 function Markdown(){
+    const marked = new Marked(
+      markedHighlight({
+        langPrefix: 'hljs language-',
+        highlight(code, lang) {
+          const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+          return hljs.highlight(code, { language }).value;
+        }
+      })
+    );
     const [markdown, setMarkdown] = useState(initialMarkdown);
-    const markedMarkdown = marked(markdown, { breaks: true });
+    const markedMarkdown = marked.parse(markdown);
+    const [expanded, setExpanded] = useState(false);
+    
+    
+      
     const handleChange=function(event){
         setMarkdown(event.target.value);
         };
-        
+
+    const handleClick=function(){
+        setExpanded(!expanded);
+        const textarea = document.getElementById('editor');
+        if (!expanded)
+        textarea.style.height = textarea.scrollHeight + 'px'; 
+        else
+        textarea.style.height = '2em'; 
+        };
+            
 return(
     <header className="markdown-header">
         <div className='markdown-container'> 
                     <div className="markdown-editor">
                 <div className="markdown-editor-toolbar">
                     <h4 className="markdown-editor-title">Markdown Editor</h4>
-                    {/* <button>Expand Preview</button> */}
+                    <button onClick={handleClick}>{expanded? 'Shrink':'Expand'}</button>
                 </div>
                 <textarea id="editor" onChange={handleChange} value={markdown}></textarea>
             </div>
-
-            <div className="markdown-editor">
+            {expanded?<></>: <div className="markdown-editor">
                 <div className="markdown-editor-toolbar">
                     <h4 className="markdown-editor-title">Markdown Output</h4>
                 </div>
-                <div className="editor"dangerouslySetInnerHTML={{ __html: markedMarkdown }}></div>
+                <div id="preview" className="editor" dangerouslySetInnerHTML={{ __html: markedMarkdown }}></div>
             </div>
-
+            }
         </div>
     </header>)}
 export default Markdown;
